a891d4e8ad48a993f4e51b8c32e51b70abc202a3
ltatemoto <luiz@jingleplay.com>
2024-03-03 22:33:49 -0800

    version1

--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('flights', include('flights.url')),
+    path('flights', include('flights.urls')),
     path('admin/', admin.site.urls),
 ]
